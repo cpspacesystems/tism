57c19aedddf2df43485a4a8a8679cc7d3be38c6f
Evan Overman <evanrileyoverman@gmail.com>
2026-02-06 19:39:49 -0800

    python wrapper

---
+++ tism-py/tism.py
@@ -0,0 +1,137 @@
+"""
+Python wrapper for TISM-C. This wrapper hides the unsafe API which is available
+in C due to the way Python would be forced to interact with C pointers.
+
+TISM works by allocating shared memory using a name, publishers can write to an
+allocation and must create one using the `tism.create()` functon. Consumers can
+then open the allocation using `tism.open()`, at which point they can read
+whatever data the publisher has written there.
+
+All TISM allocation use read/write locks, so many consumers may read an
+allocation at once, but only one writer may write at a time (only one publisher
+should ever exist anyways). Consumers may not read when a publisher is writing
+and vice versa.
+
+For Python users of TISM you must serialize you data to/from `bytes`, as Python
+has, in general, a very loose notion of the size of a type. For Rust and C the
+bytes will likely just be the raw bytes of the type the allocation is meant to
+store, and Python users should work with their `bytes` as such.
+"""
+
+
+from dataclasses import dataclass
+from _tism import ffi, lib
+
+
+@dataclass
+class TismOwnedSharedMemory:
+    """
+    A TISM shared memory allocation which this process has created. Holding an
+    instance of this class means that you are the publisher, and the only party
+    which may write to or read from the memory allocation.
+    """
+
+    shm: ffi.cdata
+
+
+    def write(self, value: bytes):
+        """
+        Write the given bytes into the shared memory allocation by cloning them.
+        This function write locks the allocation, and will block until it can do
+        so, then unlocks before returning.
+        """
+
+        if self.shm.data_len != len(value):
+            raise Exception("Given value is not the same size as allocation")
+
+        value_ptr = ffi.new("char[]", bytes(value))
+        _raise_tism_error(lib.tism_owned_write(self.shm, value_ptr))
+
+
+    def read(self) -> bytes:
+        """
+        Read from the memory allocation. This function will read lock the
+        allocation, and block until it can do so, then unlock before returning.
+        """
+
+        value_ptr = ffi.new("char[]", self.shm.data_len)
+        _raise_tism_error(lib.tism_owned_read(self.shm, value_ptr))
+        buf = ffi.buffer(value_ptr, self.shm.data_len)
+        return bytes(buf)
+
+
+@dataclass
+class TismBorrowedSharedMemory:
+    """
+    A TISM shared memory allocation which this process has opened, not created.
+    Holding an instance of this class means that you are a consumer, and can
+    only read from the allocation.
+    """
+
+    shm: lib.tism_borrowed_shared_memory_t
+
+
+    def read(self) -> bytes:
+        """
+        Read from the memory allocation. This function will read lock the
+        allocation, and block until it can do so, then unlock before returning.
+        """
+
+        value_ptr = ffi.new("char[]", self.shm.data_len)
+        _raise_tism_error(lib.tism_owned_read(self.shm, value_ptr))
+        buf = ffi.buffer(value_ptr, self.shm.data_len)
+        return bytes(buf)
+
+
+def create(name: str, init: bytes) -> TismOwnedSharedMemory:
+    """
+    Create a new TISM shared memory allocation with the given name and given
+    initial data.
+    """
+
+    c_str = _create_c_str(name)
+    init_ptr = ffi.new("char[]", bytes(init))  # use char[] as void* standin
+
+    shm = ffi.new("struct _tism_shared_memory*")
+    err = lib.tism_create(shm, c_str, init_ptr, len(init))
+
+    if err != 0:
+        raise Exception(f"TISM gave error: {err}")
+
+    return TismOwnedSharedMemory(shm)
+
+
+def open(name: str, size: int) -> TismBorrowedSharedMemory:
+    """
+    Open an existing shared memory allocation by the given name
+    """
+
+    c_str = _create_c_str(name)
+
+    shm = ffi.new("struct _tism_shared_memory*")
+    err = lib.tism_open(shm, c_str, size)
+
+    if err != 0:
+        raise Exception(f"TISM gave error: {err}")
+
+    return TismBorrowedSharedMemory(shm)
+
+
+def _raise_tism_error(err: lib.tism_result_t):
+    """
+    Check our C-style result enum errors into Python exception handling. Raises
+    an exception if the given result is not `TISM_OK`.
+    """
+
+    if err == lib.TISM_OK:
+        return
+
+    raise Exception(f"TISM gave error: {e}")
+
+
+def _create_c_str(s: str):
+    """
+    Create a C-string from the given python `str`
+    """
+
+    return ffi.new("char[]", s.encode())
---
+++ tism-py/tism_compile.py
@@ -0,0 +1,57 @@
+# This module requires the following packages:
+#  - cffi
+#    - setuptools
+
+from cffi import FFI
+import itertools
+
+TISM_HEADER_PATH: str = "../tism-c/tism.h"
+TISM_SOURCE_PATH: str = "../tism-c/tism.c"
+
+def remove_comments(source: str) -> str:
+    """
+    Remove comments from the given source, CFFI cannot parse comments.
+    """
+
+    MULTILINE_COMMENT_START: str = "/*"
+    MULTILINE_COMMENT_END: str = "*/"
+    SINGLE_LINE_COMMEND_START: str = "//"
+
+    while MULTILINE_COMMENT_START in source and MULTILINE_COMMENT_END in source:
+        start_idx = source.find(MULTILINE_COMMENT_START)
+        end_idx = source.find(MULTILINE_COMMENT_END) + len(MULTILINE_COMMENT_END)
+
+        source = source[0:start_idx] + source[end_idx:len(source)]
+
+    while SINGLE_LINE_COMMEND_START in source:
+        start_idx = source.find(SINGLE_LINE_COMMEND_START)
+        end_idx = source.find("\n") + len("\n")
+
+        source = source[0:start_idx] + source[end_idx:len(source)]
+
+    return source
+
+def replace_unknowable_types(source: str) -> str:
+    """
+    Replace types which are not known by CFFI and not defined by TISM. CFFI
+    cannot parse directives and as a result types declared in headers are
+    unusable to us. Luckily pointers are just pointers.
+    """
+
+    source = source.replace("pthread_rwlock_t*", "void*")
+    return source
+
+# Process our header a bit to make it usable for CFFI.
+header_text = replace_unknowable_types(remove_comments(open(TISM_HEADER_PATH).read()))
+header_text = '\n'.join(
+    filter(
+        lambda s: s != "" and not s.startswith("#"),  # remove directives
+        header_text.splitlines()
+    )
+)
+
+# Compile our C code.
+ffi = FFI()
+ffi.cdef(header_text)
+ffi.set_source("_tism", "#include \"" + TISM_HEADER_PATH + "\"\n", sources=[TISM_SOURCE_PATH], libraries=[])
+ffi.compile()
